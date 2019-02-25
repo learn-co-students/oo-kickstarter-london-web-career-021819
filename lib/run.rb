ff14e94e210e359188c2156ef6dcd3a7448660a9
Thomas Edwards <thce28@gmail.com>
2019-02-25 12:56:15 +0000

    Done.

---
+++ lib/run.rb
@@ -0,0 +1,24 @@
+require 'pry'
+require_relative './backer.rb'
+require_relative './project.rb'
+
+
+
+backer1 = Backer.new("Warren Buffet")
+backer2 = Backer.new("Elon Musk")
+
+project1 = Project.new("Facebook")
+project2 = Project.new("Instagram")
+
+
+
+
+
+
+
+
+
+
+binding.pry
+
+puts "h"
